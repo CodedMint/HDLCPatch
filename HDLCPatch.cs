ded18daa7675b4e37d0bc81330131aed5e7ee1da
CodedMint <59670756+CodedMint@users.noreply.github.com>
2025-01-29 12:59:59 -0600

    Fixed an issue where the mod would not function without another mod that was acting as an unknown dependency

--- HDLCPatch.cs
+++ HDLCPatch.cs
@@ -1,38 +1,75 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using HarmonyLib.Public.Patching;
+using HDLethalCompanyPatch.patches;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using UnityEngine;
 
 namespace HDLethalCompanyPatch
 {
-    [BepInPlugin("HDLCPatch", "HDLCPatch", "1.0.0")]
-    [BepInDependency("HDLethalCompany")]
+    public enum QualitySetting
+    {
+        VeryLow,
+        Low,
+        Medium,
+        High
+    }
+
+    public static class HDLCPatchProperties
+    {
+        public const string Name = "HDLCPatch";
+        public const string GUID = "HDLCPatch";
+        public const string Version = "1.0.2";
+
+        //public static QualitySetting FogQuality { get; set; } = QualitySetting.VeryLow;
+        //public static Module HDLCAssembly;
+    }
+
+    [BepInPlugin(HDLCPatchProperties.GUID, HDLCPatchProperties.Name, HDLCPatchProperties.Version)]
+    [BepInDependency("HDLethalCompany", BepInDependency.DependencyFlags.HardDependency)]
     public class HDLCPatch : BaseUnityPlugin
     {
         internal static new ManualLogSource Logger;
-        private Harmony _harmony;
+        private static Harmony _harmony;
 
         private void Awake()
         {
-            _harmony = new Harmony("HDLCPatch");
-
             // Plugin startup logic
             Logger = base.Logger;
-            Logger.LogInfo($"Plugin HDLCPatch is loaded!");
+            Logger.LogInfo($"Plugin {HDLCPatchProperties.Name} {HDLCPatchProperties.Version} is loaded!");
 
+            _harmony = new Harmony(HDLCPatchProperties.GUID);
+
+            PatchHDLC();
+        }
+
+        public static void PatchHDLC()
+        {
+            Logger.LogInfo("Patching...");
             List<MethodBase> methods = (List<MethodBase>)Harmony.GetAllPatchedMethods();
 
             foreach (MethodBase method in methods)
             {
-                Logger.LogInfo("Got method: " + method.Name + " from Module: " + method.Module);
+                PatchInfo patchi = PatchManager.GetPatchInfo(method);
 
-                if(method.Module.ToString() == "HDLethalCompanyRemake.dll" && method.Name == "RoundPostFix")
+                Patch[] prefixPatches = patchi.prefixes;
+
+                foreach (var p in prefixPatches)
                 {
-                    Logger.LogInfo("Found RoundPostFix from HDLethalCompany attempting to remove problem patch...");
-                    _harmony.Unpatch(method, HarmonyPatchType.Prefix);
+                    if (p.PatchMethod.Name == "RoundPostFix" && p.owner == "HDLethalCompany")
+                    {
+                        Logger.LogInfo("Found problem patch from HDLethalCompany RoundPostFix, unpatching it...");
+                        _harmony.Unpatch(method, p.PatchMethod);
+                    }
                 }
             }
+
+            Logger.LogInfo("Finished patching");
         }
+
     }
 }
---
+++ patches/HDLCGraphicsPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Mono.Cecil;
+using System.Reflection;
+
+
+namespace HDLethalCompanyPatch.patches
+{
+    internal class HDLCGraphicsPatch
+    {
+        [HarmonyPatch("", "SetFogQuality")]
+        [HarmonyPrefix]
+        public static void ChangeFogQuality(object __instance)
+        {
+           // __instance.GetType().GetField("m_setFogQuality", BindingFlags.Static).SetValue(__instance, (int)HDLCPatchProperties.FogQuality);
+        }
+    }
+}
