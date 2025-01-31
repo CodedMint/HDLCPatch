9e6760de43348d82968d07e3a27714d3a7191e71
CodedMint <59670756+CodedMint@users.noreply.github.com>
2025-01-31 01:32:23 -0600

    Added LethalConfig support

---
+++ config/LCHDPatchConfigSettings.cs
@@ -0,0 +1,53 @@
+﻿using LethalConfig;
+using LethalConfig.ConfigItems;
+using LethalConfig.ConfigItems.Options;
+
+namespace HDLethalCompanyPatch.config
+{
+    public static class LCHDPatchConfigSettings
+    {
+        public static BoolCheckBoxConfigItem EnableHDPatchOverrideSettingsItem;
+        public static FloatSliderConfigItem ResolutionScaleItem;
+        public static EnumDropDownConfigItem<QualitySetting> FogQualityItem;
+        public static BoolCheckBoxConfigItem EnableFogItem;
+        public static EnumDropDownConfigItem<QualitySetting> ShadowQualityItem;
+        public static EnumDropDownConfigItem<QualitySetting> LODQualityItem;
+        public static EnumDropDownConfigItem<QualitySetting> TextureQualityItem;
+        public static BoolCheckBoxConfigItem EnablePostProcessingItem;
+        public static BoolCheckBoxConfigItem EnableFoliageItem;
+        public static BoolCheckBoxConfigItem EnableResolutionOverrideItem;
+        public static BoolCheckBoxConfigItem EnableAntiAliasingItem;
+        public static BoolCheckBoxConfigItem DisableFoliageConfigItem;
+
+        public static void Setup()
+        {
+            LethalConfigManager.SetModDescription("A patch mod for HDLethalCompany that also includes runtime configs through LethalConfig");
+
+            EnableHDPatchOverrideSettingsItem = new BoolCheckBoxConfigItem(HDLCPatch.EnableHDPatchOverrideSettings, false);
+            ResolutionScaleItem = new FloatSliderConfigItem(HDLCPatch.ResolutionScale, new FloatSliderOptions { Max = 4.465f, Min = 1, RequiresRestart = false });
+            EnableFogItem = new BoolCheckBoxConfigItem(HDLCPatch.EnableFog, false);
+            FogQualityItem = new EnumDropDownConfigItem<QualitySetting>(HDLCPatch.FogQuality, false);
+            ShadowQualityItem = new EnumDropDownConfigItem<QualitySetting>(HDLCPatch.ShadowQuality, false);
+            LODQualityItem = new EnumDropDownConfigItem<QualitySetting>(HDLCPatch.LODQuality, false);
+            EnablePostProcessingItem = new BoolCheckBoxConfigItem(HDLCPatch.EnablePostProcessing, false);
+            EnableFoliageItem = new BoolCheckBoxConfigItem(HDLCPatch.EnableFoliage, false);
+            EnableResolutionOverrideItem = new BoolCheckBoxConfigItem(HDLCPatch.EnableResolutionOverride, false);
+            EnableAntiAliasingItem = new BoolCheckBoxConfigItem(HDLCPatch.EnableAntiAliasing, false);
+            TextureQualityItem = new EnumDropDownConfigItem<QualitySetting>(HDLCPatch.TextureQuality, false);
+            DisableFoliageConfigItem = new BoolCheckBoxConfigItem(HDLCPatch.DisableFoliageConfig, true);
+
+            LethalConfigManager.AddConfigItem(EnableHDPatchOverrideSettingsItem);
+            LethalConfigManager.AddConfigItem(ResolutionScaleItem);
+            LethalConfigManager.AddConfigItem(FogQualityItem);
+            LethalConfigManager.AddConfigItem(ShadowQualityItem);
+            LethalConfigManager.AddConfigItem(LODQualityItem);
+            LethalConfigManager.AddConfigItem(TextureQualityItem);
+            LethalConfigManager.AddConfigItem(EnablePostProcessingItem);
+            LethalConfigManager.AddConfigItem(EnableFogItem);
+            LethalConfigManager.AddConfigItem(EnableFoliageItem);
+            LethalConfigManager.AddConfigItem(EnableResolutionOverrideItem);
+            LethalConfigManager.AddConfigItem(EnableAntiAliasingItem);
+            LethalConfigManager.AddConfigItem(DisableFoliageConfigItem);
+        }
+    }
+}
